54fc0c9404765d6f26b3c54d05bc1745bde84dd3
Michael Osgood <michaelosgood@dhcp-v120-099.mobile.uci.edu>
2017-08-20 16:07:42 -0700

    2nd commit

--- liri.js
+++ liri.js
@@ -13,8 +13,34 @@ var accessTokenSecret = keysJS.twitterKeys.access_token_secret;
 
 // comands = ["my-tweets","spotify-this-song",
 // "movie-this","do-what-it-says"]
-var commands = process.argv[2];
+var command = process.argv[2];
+var songName = process.argv[3];
 
+if (command === 'my-tweets'){
+  //grab tweets
+}
+else if (command === "spotify-this-song"){
+  var spotify = new Spotify({
+    id: '6a4fc3cb81474ef79373b922ff0a8576',
+    secret: 'efcdea0ad29c4173a6dd0a21027050c3'
+   });
+   //receiving an error message, not sure why
+   spotify
+   .search ({ type: 'track/album/artist', query: 'My search query'})
+   .then(function(response) {
+     console.log("SPOTIFY THIS SONG");
+     console.log(response);
+   })
+   .catch(function(err) {
+     console.log(err);
+   });
+}
+else if (command === "movie-this"){
+  
+}
+else if (command === "do-what-it-says"){
+  //grab song data
+}
 // node liri.js my-tweets
 // my-tweets will show the last 20 tweets and when they were created
 // at in your bash window
@@ -24,15 +50,3 @@ var commands = process.argv[2];
 // if no song is provided, play "The Sign" by Ace of Base 
 
 
-var spotify = new Spotify({
- id: '6a4fc3cb81474ef79373b922ff0a8576',
- secret: 'efcdea0ad29c4173a6dd0a21027050c3'
-});
-
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
- if (err) {
-   return console.log('Error occurred: ' + err);
- }
-
-console.log(data); 
-});
